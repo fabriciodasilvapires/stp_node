98beb179c28586b6dee4a4663c430b8c614d71f9
fabriciodasilvapires <fabriciopires@discente.ufg.br>
2024-12-07 00:02:06 -0300

    Update express.d.ts

--- middlewares/express.d.ts
+++ middlewares/express.d.ts
@@ -2,6 +2,6 @@ import { JwtPayload } from 'jsonwebtoken';
 
 declare module 'express-serve-static-core' {
   interface Request {
-    user?: JwtPayload | string;
+   user?: JwtPayload | string;
   }
 }
